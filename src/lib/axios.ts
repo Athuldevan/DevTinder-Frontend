86f0c85e64a8c4e088735cc593f570bb99b70cfd
Athul <athuldevan90@gmail.com>
2025-11-05 23:22:03 +0530

    fix: bug fix in base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let baseURL = "/api/v1";
+let baseURL = "/api";
 
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
    baseURL = "http://127.0.0.1:8000/api/v1";
